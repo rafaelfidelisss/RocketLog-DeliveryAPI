3f8c541f46c18347657251d4bc99ad3357034c52
Rafael Fidelis <rafafbortolato@gmail.com>
2025-08-29 15:15:45 -0300

    create request validation with zod, and hashed password

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express"
+import { hash } from "bcrypt"
+import { z } from "zod"
 
 class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "okei" })
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(8),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    const hashedPassword = await hash(password, 8)
+
+
+    return response.json({ message: "okei", hashedPassword })
   }
 }
 
