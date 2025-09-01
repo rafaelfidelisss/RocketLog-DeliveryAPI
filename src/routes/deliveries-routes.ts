1638b0b7578b4c3b9d8ac47b9611e905ce449d38
Rafael Fidelis <rafafbortolato@gmail.com>
2025-09-01 17:58:42 -0300

    create update delivery status function

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,14 +1,19 @@
 import { Router } from "express"
+
 import { DeliveriesController } from "@/controllers/deliveries-controller"
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller"
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
 import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization"
 
 const deliveriesRoutes = Router()
 const deliveriesController = new DeliveriesController()
+const deliveriesStatusController = new DeliveriesStatusController()
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]))
 deliveriesRoutes.post("/", deliveriesController.create)
 deliveriesRoutes.get("/", deliveriesController.index)
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update)
 
 export { deliveriesRoutes }
