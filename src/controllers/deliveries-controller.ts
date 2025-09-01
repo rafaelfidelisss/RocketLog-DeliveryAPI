e8773578f74f155f0d2d9c5f2a4c606d3779abb1
Rafael Fidelis <rafafbortolato@gmail.com>
2025-09-01 17:42:52 -0300

    create index function for deliveries

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,11 +1,37 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
 
 class DeliveriesController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string(),
+    });
 
+    const { user_id, description } = bodySchema.parse(request.body);
 
-    return response.json({ message: "okei" })
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description,
+      },
+    });
+
+    return response.status(201).json({ message: "okei" });
+  }
+
+  async index(request: Request, response: Response) {
+    const deliveries = await prisma.delivery.findMany({
+      include: {
+        user: {
+          select: { name: true, email: true },
+        },
+      },
+    });
+
+    return response.json(deliveries);
   }
 }
 
-export { DeliveriesController }
+export { DeliveriesController };
--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -9,5 +9,6 @@ const deliveriesController = new DeliveriesController()
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]))
 deliveriesRoutes.post("/", deliveriesController.create)
+deliveriesRoutes.get("/", deliveriesController.index)
 
 export { deliveriesRoutes }
